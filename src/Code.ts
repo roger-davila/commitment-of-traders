f03391418b837914dbd05473b33a05b44bc6bed3
Roger Davila <rogerddavila@gmail.com>
2023-06-07 00:58:08 -0700

    move ui declaration into onOpen function

--- src/Code.ts
+++ src/Code.ts
@@ -5,14 +5,12 @@
 // // Bonus feature: allow user to select which futures to display/keep track of
 // Only pull certain columns of the total data source
 
-const spreadsheet: GoogleAppsScript.Spreadsheet.Spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
-const ui: GoogleAppsScript.Base.Ui = SpreadsheetApp.getUi();
-
 function menu1(): void {
   Logger.log('Hello');
 }
 
 function onOpen(): void {
+  const ui: GoogleAppsScript.Base.Ui = SpreadsheetApp.getUi();
   ui.createMenu('COT')
     .addItem('Menu Item 1', 'menu1')
     .addToUi();
