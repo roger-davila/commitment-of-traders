a476486a87635dc569bc78872cd28e11bf0bf088
Roger Davila <rogerddavila@gmail.com>
2023-06-13 23:49:52 -0700

    update list of urls to include socrata api

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
 namespace Constants {
   export const URL_HISTORICAL_DATA = 'https://www.cftc.gov/files/dea/history/deacot2023.zip';
   export const URL_WEEKLY_DATA = 'https://www.cftc.gov/dea/newcot/deafut.txt';
+  export const URL_HISTORICAL_DATA_SOCRATA = "https://publicreporting.cftc.gov/resource/6dca-aqww.json";
 }
