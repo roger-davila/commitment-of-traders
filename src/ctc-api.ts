c4baed7601b358f1ca30ab4c75747d5b4c45a89d
Roger Davila <rogerddavila@gmail.com>
2023-06-13 23:52:09 -0700

    condense commitment of traders report type properties and streamline api functions

--- src/ctc-api.ts
+++ src/ctc-api.ts
@@ -5,11 +5,9 @@
  * @returns void
  */
 function getWeeklyData(url: string, indices?: string[]): void {
-  const dataBlob: GoogleAppsScript.Base.Blob | void = sendRequest(url);
-  if (dataBlob) {
-    const weeklyData = dataBlob.getDataAsString().split('\n').map((report) => report.split(','));
+  const data: object[] | void = sendRequest(url);
+  if (data) Logger.log(data.length);
     // Logger.log(weeklyData);
-  }
 }
 
 /** Get all data for the specified indices
@@ -18,16 +16,10 @@ function getWeeklyData(url: string, indices?: string[]): void {
  * @param {string[]} indices List of indices to get data for
  * @returns void
  */
-function getAllData(url: string, indices?: string[]) : void {
-  const dataBlob: GoogleAppsScript.Base.Blob | void = sendRequest(url);
-  if (dataBlob) {
-    const historicalData: string[] = Utilities.unzip(dataBlob)[0].getDataAsString().split('\n');
-    const headers: string[] = historicalData[0].split(',');
-    const parsedHeaders = parseColumnHeaders(headers);
-    const cotReports: string[][] = historicalData.slice(1).map((report) => report.split(','));
-    const processedCotReports = processCOTReports(cotReports, parsedHeaders);
-    Logger.log(processedCotReports);
-  }
+
+function getAllData(url: string): void {
+  const data: object[] | void = sendRequest(url);
+  if (data) Logger.log(data);
 }
 
 function parseColumnHeaders(headers: string[]): string[] {
@@ -55,138 +47,25 @@ function processCOTReports(cotReports: string[][], columnHeaders: string[]): Com
 }
 
 function test() {
-  getWeeklyData(Constants.URL_WEEKLY_DATA);
-  getAllData(Constants.URL_HISTORICAL_DATA);
+  getWeeklyData(Constants.URL_HISTORICAL_DATA_SOCRATA);
+  getAllData(Constants.URL_HISTORICAL_DATA_SOCRATA);
 }
 
 type CommitmentOfTradersReport = {
-  marketAndExchangeNames: string;
-  asOfDateInFormYYMMDD: string;
-  asOfDateInFormYYYYMMDD: string;
-  cftcContractMarketCode: string;
-  cftcMarketCodeInInitials: string;
-  cftcRegionCode: string;
-  cftcCommodityCode: string;
-  openInterestAll: number;
-  noncommercialPositionsLongAll: number;
-  noncommercialPositionsShortAll: number;
-  noncommercialPositionsSpreadingAll: number;
-  commercialPositionsLongAll: number;
-  commercialPositionsShortAll: number;
-  totalReportablePositionsLongAll: number;
-  totalReportablePositionsShortAll: number;
-  nonreportablePositionsLongAll: number;
-  nonreportablePositionsShortAll: number;
-  openInterestOld: number;
-  noncommercialPositionsLongOld: number;
-  noncommercialPositionsShortOld: number;
-  noncommercialPositionsSpreadingOld: number;
-  commercialPositionsLongOld: number;
-  commercialPositionsShortOld: number;
-  totalReportablePositionsLongOld: number;
-  totalReportablePositionsShortOld: number;
-  nonreportablePositionsLongOld: number;
-  nonreportablePositionsShortOld: number;
-  openInterestOther: number;
-  noncommercialPositionsLongOther: number;
-  noncommercialPositionsShortOther: number;
-  noncommercialPositionsSpreadingOther: number;
-  commercialPositionsLongOther: number;
-  commercialPositionsShortOther: number;
-  totalReportablePositionsLongOther: number;
-  totalReportablePositionsShortOther: number;
-  nonreportablePositionsLongOther: number;
-  nonreportablePositionsShortOther: number;
-  changeInOpenInterestAll: number;
-  changeInNoncommercialLongAll: number;
-  changeInNoncommercialShortAll: number;
-  changeInNoncommercialSpreadingAll: number;
-  changeInCommercialLongAll: number;
-  changeInCommercialShortAll: number;
-  changeInTotalReportableLongAll: number;
-  changeInTotalReportableShortAll: number;
-  changeInNonreportableLongAll: number;
-  changeInNonreportableShortAll: number;
-  percentOfOpenInterestOIAll: number;
-  percentOfOINoncommercialLongAll: number;
-  percentOfOINoncommercialShortAll: number;
-  percentOfOINoncommercialSpreadingAll: number;
-  percentOfOICommercialLongAll: number;
-  percentOfOICommercialShortAll: number;
-  percentOfOITotalReportableLongAll: number;
-  percentOfOITotalReportableShortAll: number;
-  percentOfOINonreportableLongAll: number;
-  percentOfOINonreportableShortAll: number;
-  percentOfOpenInterestOIOld: number;
-  percentOfOINoncommercialLongOld: number;
-  percentOfOINoncommercialShortOld: number;
-  percentOfOINoncommercialSpreadingOld: number;
-  percentOfOICommercialLongOld: number;
-  percentOfOICommercialShortOld: number;
-  percentOfOITotalReportableLongOld: number;
-  percentOfOITotalReportableShortOld: number;
-  percentOfOINonreportableLongOld: number;
-  percentOfOINonreportableShortOld: number;
-  percentOfOpenInterestOIOther: number;
-  percentOfOINoncommercialLongOther: number;
-  percentOfOINoncommercialShortOther: number;
-  percentOfOINoncommercialSpreadingOther: number;
-  percentOfOICommercialLongOther: number;
-  percentOfOICommercialShortOther: number;
-  percentOfOITotalReportableLongOther: number;
-  percentOfOITotalReportableShortOther: number;
-  percentOfOINonreportableLongOther: number;
-  percentOfOINonreportableShortOther: number;
-  tradersTotalAll: number;
-  tradersNoncommercialLongAll: number;
-  tradersNoncommercialShortAll: number;
-  tradersNoncommercialSpreadingAll: number;
-  tradersCommercialLongAll: number;
-  tradersCommercialShortAll: number;
-  tradersTotalReportableLongAll: number;
-  tradersTotalReportableShortAll: number;
-  tradersTotalOld: number;
-  tradersNoncommercialLongOld: number;
-  tradersNoncommercialShortOld: number;
-  tradersNoncommercialSpreadingOld: number;
-  tradersCommercialLongOld: number;
-  tradersCommercialShortOld: number;
-  tradersTotalReportableLongOld: number;
-  tradersTotalReportableShortOld: number;
-  tradersTotalOther: number;
-  tradersNoncommercialLongOther: number;
-  tradersNoncommercialShortOther: number;
-  tradersNoncommercialSpreadingOther: number;
-  tradersCommercialLongOther: number;
-  tradersCommercialShortOther: number;
-  tradersTotalReportableLongOther: number;
-  tradersTotalReportableShortOther: number;
-  concentrationGrossLT4TDRLongAll: number;
-  concentrationGrossLT4TDRShortAll: number;
-  concentrationGrossLT8TDRLongAll: number;
-  concentrationGrossLT8TDRShortAll: number;
-  concentrationNetLT4TDRLongAll: number;
-  concentrationNetLT4TDRShortAll: number;
-  concentrationNetLT8TDRLongAll: number;
-  concentrationNetLT8TDRShortAll: number;
-  concentrationGrossLT4TDRLongOld: number;
-  concentrationGrossLT4TDRShortOld: number;
-  concentrationGrossLT8TDRLongOld: number;
-  concentrationGrossLT8TDRShortOld: number;
-  concentrationNetLT4TDRLongOld: number;
-  concentrationNetLT4TDRShortOld: number;
-  concentrationNetLT8TDRLongOld: number;
-  concentrationNetLT8TDRShortOld: number;
-  concentrationGrossLT4TDRLongOther: number;
-  concentrationGrossLT4TDRShortOther: number;
-  concentrationGrossLT8TDRLongOther: number;
-  concentrationGrossLT8TDRShortOther: number;
-  concentrationNetLT4TDRLongOther: number;
-  concentrationNetLT4TDRShortOther: number;
-  concentrationNetLT8TDRLongOther: number;
-  concentrationNetLT8TDRShortOther: number;
-  contractUnits: string;
-  cftcContractMarketCodeQuotes: string;
-  cftcMarketCodeInInitialsQuotes: string;
-  cftcCommodityCodeQuotes: string;
+  id: string; // id
+  marketAndExchangeNames: string; // market_and_exchange_names
+  date: string; // report_date_as_yyyy_mm_dd
+  nonCommercialPositionsLongAll: number; // noncomm_positions_long_all
+  nonCommercialPositionsShortAll: number; // noncomm_positions_short_all
+  commercialPositionsLongAll: number; // comm_positions_long_all
+  commercialPositionsShortAll: number; // comm_positions_short_all
+  changeInNonCommercialLongAll: number; // // change_in_noncomm_long_all
+  changeInNonCommercialShortAll: number; // change_in_noncomm_short_all
+  changeInCommercialLongAll: number; // change_in_comm_long_all
+  changeInCommercialShortAll: number; // change_in_comm_short_all
+  percentOfOiNonCommercialLongAll: number;  //pct_of_oi_noncomm_long_all
+  percentOfOiNonCommercialShortAll: number; //pct_of_oi_noncomm_short_all
+  percentOfOiCommercialLongAll: number; //pct_of_oi_comm_long_all
+  percentOfOiCommercialShortAll: number; //pct_of_oi_comm_short_all
+  contractUnits: string; // contract_units
 }
