4cf767146972294f6261739b1bca22ae3a5d4025
Roger Davila <rogerddavila@gmail.com>
2023-06-13 23:50:52 -0700

    update sendRequest to check for api keys and then send a basic authroization header

--- src/send-request.ts
+++ src/send-request.ts
@@ -6,19 +6,29 @@ namespace Requests {
  * @param {string} url URL endpoint for HTTP request
  * @param {GoogleAppsScript.URL_Fetch.HttpMethod} method HTTP request type GET by default
  * @param {any} payload Payload data to send
- * @returns {GoogleAppsScript.Base.Blob | void} Data payload in the form of a blob or response text
+ * @returns {object[] | void} Data payload in the form of a parsed JSON object or response text
  * @throws {Error} Bad Request
  */
-function sendRequest(url: string, method: GoogleAppsScript.URL_Fetch.HttpMethod = 'get', payload: any = null): GoogleAppsScript.Base.Blob | void {
-  const advancedParameters: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = { method };
+function sendRequest(url: string, method: GoogleAppsScript.URL_Fetch.HttpMethod = 'get', payload: any = null): object[] | void {
+  // Set up fetch options
+  const options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = { method };
   if (payload) {
-    advancedParameters.contentType = 'application/json';
-    advancedParameters.payload = JSON.stringify(payload);
+    options.contentType = 'application/json';
+    options.payload = JSON.stringify(payload);
   }
+  const apiKeyId: string | null = PropertiesService.getScriptProperties().getProperty('API_KEY_ID');
+  const apiKeySecret: string | null = PropertiesService.getScriptProperties().getProperty('API_KEY_SECRET');
+  if (apiKeyId && apiKeySecret) {
+    options.headers = options.headers || {};
+    // Default authorization for Socrata API is 'Basic'
+    options.headers.Authorization = `Basic ${Utilities.base64Encode(`${apiKeyId}:${apiKeySecret}`)}`;
+  }
+  Logger.log(options);
+  // Finish setting up fetch options
   try {
-    let response: GoogleAppsScript.URL_Fetch.HTTPResponse = UrlFetchApp.fetch(url, advancedParameters);
+    let response: GoogleAppsScript.URL_Fetch.HTTPResponse = UrlFetchApp.fetch(url, options);
     if (response.getResponseCode() < 300 && response.getResponseCode() >= 200)
-      return response.getBlob();
+      return JSON.parse(response.getContentText()) as object[];
   } catch (error) {
     throw new Error(`${error}`);
   }
