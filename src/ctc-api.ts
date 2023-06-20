3da5c03d6c15cd12ac9cff92383939c5f6ee02df
Roger Davila <rogerddavila@gmail.com>
2023-06-19 23:47:07 -0700

    create converter functions to parse json data into COT data types

--- src/ctc-api.ts
+++ src/ctc-api.ts
@@ -1,24 +1,28 @@
-/** Get weekly data for the specified indices
+/** Get weekly data for all or specific indices
  * 
- * @param {string} url Endpoint for HTTP GET request
  * @param {string[]} indices List of indices to get data for
- * @returns void
+ * @returns {CommitmentOfTradersReport[] || void}
  */
-function getWeeklyData(): object[] | void {
+function getWeeklyData(indices?: string[]): CommitmentOfTradersReport[] | void {
   const data: object[] | void = sendRequest(Constants.URL_HISTORICAL_DATA_SOCRATA);
-  if (data) return data;
+  if (data) {
+    return processCOTReports(data as object[])
+    .filter((report) => report != undefined) as CommitmentOfTradersReport[];
+  }
 }
 
-/** Get all data for the specified indices
+/** Get all data for all or specific indices
  * 
- * @param {string} url Endpoint for HTTP GET request
  * @param {string[]} indices List of indices to get data for
- * @returns void
+ * @returns {CommitmentOfTradersReport[] || void}
  */
 
-function getAllData(): object[] | void {
+function getAllData(indices?: string[]): CommitmentOfTradersReport[] | void {
   const data: object[] | void = sendRequest(Constants.URL_HISTORICAL_DATA_SOCRATA);
-  if (data) return data;
+  if (data) {
+    return processCOTReports(data as object[])
+      .filter((report) => report != undefined) as CommitmentOfTradersReport[];
+  }
 }
 
 function parseColumnHeaders(headers: string[]): string[] {
@@ -32,21 +36,32 @@ function parseColumnHeaders(headers: string[]): string[] {
   });
 }
 
-function processCOTReports(cotReports: string[][], columnHeaders: string[]): CommitmentOfTradersReport[] {
-  return cotReports.reduce((reports, report) => {
-    if (report.length != columnHeaders.length) return reports;
-    else {
-      const cotReport: CommitmentOfTradersReport = report.reduce((cotReport, property, index) => {
-        return {...cotReport, [columnHeaders[index]]: property.trim()}
-      }, {} as CommitmentOfTradersReport);
-      reports.push(cotReport);
-      return reports;
-    }
-  }, [] as CommitmentOfTradersReport[]);
+function processCOTReports(reports: object[]): (CommitmentOfTradersReport | undefined)[] {
+  return reports.map((report) => processCOTReport(report));
+}
+
+function processCOTReport(report: object): CommitmentOfTradersReport | undefined {
+  return report ? {
+    id: report['id'],
+    marketAndExchangeNames: report['market_and_exchange_names'],
+    date: report['report_date_as_yyyy_mm_dd'],
+    nonCommercialPositionsLongAll: report['noncomm_positions_long_all'],
+    nonCommercialPositionsShortAll: report['noncomm_positions_short_all'],
+    commercialPositionsLongAll: report['comm_positions_long_all'],
+    commercialPositionsShortAll: report['comm_positions_short_all'],
+    changeInNonCommercialLongAll: report['change_in_noncomm_long_all'],
+    changeInNonCommercialShortAll: report['change_in_noncomm_short_all'],
+    changeInCommercialLongAll: report['change_in_comm_long_all'],
+    changeInCommercialShortAll: report['change_in_comm_short_all'],
+    percentOfOiNonCommercialLongAll: report['pct_of_oi_noncomm_long_all'],
+    percentOfOiNonCommercialShortAll: report['pct_of_oi_noncomm_short_all'],
+    percentOfOiCommercialLongAll: report['pct_of_oi_comm_long_all'],
+    percentOfOiCommercialShortAll: report['pct_of_oi_comm_short_all'],
+    contractUnits: report['contract_units']
+  } : undefined;
 }
 
 function test() {
-  getWeeklyData();
   getAllData();
 }
 
